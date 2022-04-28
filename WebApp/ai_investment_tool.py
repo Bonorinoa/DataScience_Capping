a97a1f77b28f5568d628fc31dc6985cb852edbad
Augusto Gonzalez Bonorino <Augusto.GonzalezBonorino1@marist.edu>
2022-04-28 11:22:56 -0400

    Update ai_investment_tool.py

--- WebApp/ai_investment_tool.py
+++ WebApp/ai_investment_tool.py
@@ -53,8 +53,11 @@ from scipy.special import softmax
 import datetime
 
 from dateutil import parser
+from utils import chart, db
 
 timestr = time.strftime("%Y%m%d")
+COMMENT_TEMPLATE_MD = """{} - {}
+> {}"""
 
 ######## FUNCTIONS ########
 
@@ -1122,3 +1125,39 @@ if st.checkbox("Run Monte Carlo Portfolio Optimization algorithm"):
                 submitted = st.form_submit_button("Submit")
                 if submitted:
                   st.success("Loading data!")
+                
+# Comments part
+
+conn = db.connect()
+comments = db.collect(conn)
+
+with st.expander("💬 Open comments"):
+
+    # Show comments
+
+    st.write("**Comments:**")
+
+    for index, entry in enumerate(comments.itertuples()):
+        st.markdown(COMMENT_TEMPLATE_MD.format(entry.name, entry.date, entry.comment))
+
+        is_last = index == len(comments) - 1
+        is_new = "just_posted" in st.session_state and is_last
+        if is_new:
+            st.success("☝️ Your comment was successfully posted.")
+
+    space(2)
+
+    # Insert comment
+
+    st.write("**Add your own comment:**")
+    form = st.form("comment")
+    name = form.text_input("Name")
+    comment = form.text_area("Comment")
+    submit = form.form_submit_button("Add comment")
+
+    if submit:
+        date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        db.insert(conn, [[name, comment, date]])
+        if "just_posted" not in st.session_state:
+            st.session_state["just_posted"] = True
+        st.experimental_rerun()
